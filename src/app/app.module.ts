42bb6708bfcc09b637af0c52333a8ff8d05b5b48
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-05 17:53:27 -0400

    Split Navbar and Intro Hero into Components Bold brand text Center hero text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroHeroComponent } from './intro-hero/intro-hero.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, IntroHeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
