ce7c72dca4c37303cab93f28f606edb50b2dbe81
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-16 19:27:39 -0400

    Make Link Buttons Functional

--- src/app/info-card/link-button/link-button.component.ts
+++ src/app/info-card/link-button/link-button.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-link-button',
   templateUrl: './link-button.component.html',
-  styleUrls: ['./link-button.component.css']
+  styleUrls: ['./link-button.component.css'],
 })
 export class LinkButtonComponent implements OnInit {
+  @Input() link: string;
+  @Input() linkName: string;
+  @Input() bulmaColor: string;
+  @Input() faIcon: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // So errors aren't thrown if the color isn't specified.
+    if (this.bulmaColor == null) {
+      this.bulmaColor = '';
+    }
   }
-
 }
