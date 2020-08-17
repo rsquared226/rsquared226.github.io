21319551cedec8d5d152e4b7438b274dbf02a3f1
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-17 15:33:58 -0400

    Create Portfolio Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FooterComponent } from './footer/footer.component';
 import { LeadershipComponent } from './leadership/leadership.component';
 import { InfoCardComponent } from './info-card/info-card.component';
 import { LinkButtonComponent } from './info-card/link-button/link-button.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, IntroHeroComponent, FooterComponent, LeadershipComponent, InfoCardComponent, LinkButtonComponent],
+  declarations: [AppComponent, NavbarComponent, IntroHeroComponent, FooterComponent, LeadershipComponent, InfoCardComponent, LinkButtonComponent, PortfolioSectionComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
