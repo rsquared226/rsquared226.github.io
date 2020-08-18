b08bed0c9335e3bd3cd6242c58cdcabb2b3c5098
Rahul Ramkumar <rahulram226@gmail.com>
2020-08-18 11:07:28 -0400

    Make Basic Portfolio Section

--- src/app/leadership/leadership.component.ts
+++ src/app/leadership/leadership.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoCardDetails } from '../portfolio-section/portfolio-section.component';
 
 @Component({
   selector: 'app-leadership',
   templateUrl: './leadership.component.html',
-  styleUrls: ['./leadership.component.css']
+  styleUrls: ['./leadership.component.css'],
 })
 export class LeadershipComponent implements OnInit {
+  infoCardDetails: InfoCardDetails[] = [
+    {
+      imageUrl: 'https://hackpsu.org/assets/images/logo-daytime.svg',
+      title: 'HackPSU',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan ligula ut mi vehicula, in pulvinar orci.',
+      skills: 'C++, Java',
+      githubLink: '#',
+    },
+    {
+      imageUrl: 'https://hackpsu.org/assets/images/logo-daytime.svg',
+      title: 'ASDF',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan ligula ut mi vehicula, in pulvinar orci iaculis. Nunc ut aliquam neque. Curabitur id justo nec elit ultricies consequat. Nunc nec neque eget purus tempus aliquet. Fusce accumsan nibh leo, a euismod arcu pharetra nec. Proin feugiat metus quis imperdiet euismod. Mauris nunc urna, facilisis ac feugiat nec, interdum et diam. Praesent a scelerisque odio.',
+      skills: 'C++, Java',
+      generalLink: 'https://hackpsu.org/',
+      generalLinkName: 'Website',
+      generalFaIcon: 'fas fa-desktop',
+    },
+    {
+      imageUrl: 'https://hackpsu.org/assets/images/logo-daytime.svg',
+      title: 'Anothertest',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan ligula ut mi vehicula, in pulvinar orci iaculis. Nunc ut aliquam neque. Curabitur id justo nec elit ultricies consequat. Nunc nec neque eget purus tempus aliquet. Fusce accumsan nibh leo, a euismod arcu pharetra nec. Proin feugiat metus quis imperdiet euismod. Mauris nunc urna, facilisis ac feugiat nec, interdum et diam. Praesent a scelerisque odio.',
+      skills: 'C++, Java',
+      generalLink: 'https://hackpsu.org/',
+      generalLinkName: 'Website',
+      generalFaIcon: 'fas fa-desktop',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+export interface InfoCardDetails {
+  readonly imageUrl?: string;
+  readonly title: string;
+  readonly role?: string;
+  readonly description?: string;
+  readonly skills?: string;
+  readonly githubLink?: string;
+  readonly generalLink?: string;
+  readonly generalLinkName?: string;
+  readonly generalFaIcon?: string;
+}
 @Component({
   selector: 'app-portfolio-section',
   templateUrl: './portfolio-section.component.html',
-  styleUrls: ['./portfolio-section.component.css']
+  styleUrls: ['./portfolio-section.component.css'],
 })
 export class PortfolioSectionComponent implements OnInit {
+  @Input() sectionName: string;
+  @Input() bulmaColor: string;
+  @Input() infoCardsDetails: InfoCardDetails[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  firstColData(): InfoCardDetails[] {
+    return this.infoCardsDetails.filter((_, index) => index % 2 === 0);
   }
 
+  secondColData(): InfoCardDetails[] {
+    return this.infoCardsDetails.filter((_, index) => index % 2 !== 0);
+  }
 }
