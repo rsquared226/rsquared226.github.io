35a79033147fd7861326d7306251b4a9210a8270
Rahul Ramkumar <rahulram226@gmail.com>
2020-09-05 18:45:19 -0400

    Start Experience Section Also fix text colors bug with info card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { LeadershipComponent } from './leadership/leadership.component';
 import { InfoCardComponent } from './info-card/info-card.component';
 import { LinkButtonComponent } from './info-card/link-button/link-button.component';
 import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, IntroHeroComponent, FooterComponent, LeadershipComponent, InfoCardComponent, LinkButtonComponent, PortfolioSectionComponent],
+  declarations: [AppComponent, NavbarComponent, IntroHeroComponent, FooterComponent, LeadershipComponent, InfoCardComponent, LinkButtonComponent, PortfolioSectionComponent, ExperienceComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
