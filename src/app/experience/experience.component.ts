0c5e470a0bae4cab5636a1b8e95fc46a4cc3885d
Rahul Ramkumar <rahulram226@gmail.com>
2020-09-05 20:07:59 -0400

    Add Budget My Life

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -16,55 +16,20 @@ import { InfoCardDetails } from '../info-card/info-card.component';
 export class ExperienceComponent implements OnInit {
   readonly infoCardDetails: InfoCardDetails[] = [
     {
-      imageUrl: '/assets/images/HackPSU.jpg',
-      title: 'HackPSU',
-      role: 'Tech Director',
-      dates: 'January 2020 - Current',
+      imageUrl: '/assets/images/budgetmylife.jpg',
+      title: 'Budget My Life',
+      dates: 'May 2020 - Current',
       descriptionPoints: [
-        'Manage a team of 15 to develop and maintain an entire technology stack to allow 1000+ hackathon participants to create profiles, register for the event, and find relevant information',
-        'Delegate information using the Agile methodology in conjunction with Asana',
+        'Designed, coded, and deployed a beautiful and intuitive budgeting application using Flutter',
+        'Enable the user to track transactions and view insights on how they spend/earn their money',
+        '200+ installs',
       ],
-      generalLink: 'https://hackpsu.org/',
-      generalLinkName: 'Website',
-      generalFaIcon: 'fas fa-desktop',
-      githubLink: 'https://github.com/Hack-PSU',
-    },
-    {
-      imageUrl: '/assets/images/unicef.jpg',
-      title: 'UNICEF',
-      role: 'Treasurer, Fundraising Chair',
-      dates: 'January 2020 - Current',
-      descriptionPoints: [
-        'Responsible for the club budget and sent proceeds from fundraisers to UNICEF',
-        'Communicated with 4 businesses for fundraisers (raised $900+) and for organizing volunteering events',
-        'Worked together with the officer team of 8 to research and present current world issues to 150+ club members',
-      ],
-      generalLink: 'https://www.instagram.com/unicefpennstate/',
-      generalLinkName: 'Social',
-      generalFaIcon: 'fab fa-instagram',
-    },
-    {
-      title: 'FIRST Tech Challenge',
-      role: 'Software Lead',
-      dates: 'June 2017 - May 2019',
-      descriptionPoints: [
-        'Led team of 6 developers to program the robot to compete in competitions',
-        'Taught new members the basics of Java and Android Studio',
-        'Assigned tasks to members using Basecamp and used GitHub to manage the code',
-      ],
-      generalLink: 'http://www.pacar-robotics.org/',
-      generalLinkName: 'Website',
-      generalFaIcon: 'fas fa-desktop',
-      githubLink: 'https://github.com/pacar-robotics',
-    },
-    {
-      title: 'Technology Student Association',
-      role: 'Chapter President, Secretary',
-      dates: 'August 2017 - May 2019',
-      descriptionPoints: [
-        'Led the school chapter to ensure all 96 members knew how to register and compete effectively in their events',
-        'Raised over $500 for the club by marketing Little Debbie’s treats to the school',
-      ],
+      generalLink:
+        'https://play.google.com/store/apps/details?id=rsquared226.budget_my_life',
+      generalLinkName: 'Play Store',
+      generalFaIcon: 'fab fa-google-play',
+      githubLink: 'https://github.com/rsquared226/budget_my_life',
+      skills: 'Flutter, SQLite, CI/CD, Dart, Figma, Mobile app development',
     },
   ];
 
