ac9bede45d45291b4d3327b2ee0131e4e263cc6e
Rahul Ramkumar <rahulram226@gmail.com>
2020-09-05 21:34:05 -0400

    Add Website and HackPSU Projects

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -29,7 +29,34 @@ export class ExperienceComponent implements OnInit {
       generalLinkName: 'Play Store',
       generalFaIcon: 'fab fa-google-play',
       githubLink: 'https://github.com/rsquared226/budget_my_life',
-      skills: 'Flutter, SQLite, CI/CD, Dart, Figma, Mobile app development',
+      skills: 'Flutter, Dart, SQLite, CI/CD, Figma, Mobile app dev',
+    },
+    {
+      imageUrl: '/assets/images/HackPSU.jpg',
+      title: 'HackPSU Mobile App',
+      dates: 'October 2019 - Current',
+      descriptionPoints: [
+        'Designing and building HackPSU’s first mobile app using React Native',
+        'Allow 1000+ users to query backend database to get user data',
+      ],
+      githubLink: 'https://github.com/Hack-PSU/frontend-app/',
+      skills: 'React Native, Typescript, Figma, Mobile app dev',
+    },
+    {
+      imageUrl: '/assets/images/websitepreview.png',
+      title: 'Personal Portfolio',
+      dates: 'July 2020 - Current',
+      descriptionPoints: [
+        'This website!',
+        'Designed and coded this portfolio using Angular and Bulma',
+        "Utilize Angular's object-oriented values to prevent repeated code in HTML by using components for elements such as info cards",
+      ],
+      skills:
+        'Angular, Typescript, HTML, CSS, Bulma, Figma, GitHub Pages, Google Domains',
+      generalLink: 'https://rahulramkumar.dev/',
+      generalLinkName: 'Website',
+      generalFaIcon: 'fas fa-desktop',
+      githubLink: 'https://github.com/rsquared226/rsquared226.github.io',
     },
   ];
 
